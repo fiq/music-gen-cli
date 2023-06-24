c9948c021acd5cadfc852a1903dd4e67ffd097e2
Raf G <rafiq@dreamthought.com>
2023-06-24 22:47:29 +1200

    feature: lsImposed a 30 second limit

--- music-gen-cli.py
+++ music-gen-cli.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 import os
 import argparse as ap
 import sounddevice as sd
@@ -7,6 +8,8 @@ import torchaudio
 from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 
+
+MAX_SECONDS = 30
 parser = ap.ArgumentParser(
     prog = "musicgen",
     description = "Generates music based on keywords",
@@ -15,8 +18,8 @@ parser = ap.ArgumentParser(
 parser.add_argument('-k', '--keyword', nargs='+', help="<Required> prompt keyword", required=True)
 parser.add_argument('-f', '--fileprefix', type=str, help="<Required> output file prefix", required=True)
 parser.add_argument('-p', '--play', action='store_true', help="<Required> output file prefix" )
+parser.add_argument('-s', '--seconds', type=int, help="<Required> seconds to record - defaults to 5s. Capped at 30s", default=5 )
 args = parser.parse_args()
-
 
 def play_audio(output_file):
     # Extract data and sampling rate from file
@@ -24,8 +27,8 @@ def play_audio(output_file):
     sd.play(data, fs)
     status = sd.wait()  # Wait until file is done playing
 
-def gen_music(model, descriptions):
-    model.set_generation_params(duration=5) #seconds
+def gen_music(model, descriptions, seconds):
+    model.set_generation_params(duration=seconds) #seconds
     #wav = model.generate_unconditional(2) #no idea
     return model.generate(descriptions)
 
@@ -33,10 +36,20 @@ def write_music(sample_rate, file_prefix, wav_model):
     for idx, one_wav in enumerate(wav_model):
         audio_write(f'{idx}_{file_prefix}', one_wav.cpu(), sample_rate, strategy="loudness")
 
+def get_duration(seconds):
+    if seconds > MAX_SECONDS:
+        print(f'Capping at {MAX_SECONDS} seconds', file=sys.stderr)
+        return MAX_SECONDS
+    else:
+        return seconds
+
 file_prefix = args.fileprefix
 descriptions = args.keyword
+seconds = get_duration(args.seconds)
+
+# Generate music
 model = MusicGen.get_pretrained('small')
-wav_model = gen_music(model, descriptions)
+wav_model = gen_music(model, descriptions, seconds)
 write_music(model.sample_rate, file_prefix, wav_model)
 os.replace(f'0_{file_prefix}.wav', f'{file_prefix}.wav')
 
