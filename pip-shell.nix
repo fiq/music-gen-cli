f35e5e7eba6575e55b1a65ac15c27dc2061b73d9
Raf Gemmail <rafiq@dreamthought.com>
2023-07-01 09:59:28 +1200

    added flake

---
+++ pip-shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSUserEnv {
+  name = "pipzone";
+  targetPkgs = pkgs: (with pkgs; [
+    python310
+    python310Packages.pip
+    python310Packages.virtualenv
+    portaudio
+    cudaPackages.cudatoolkit
+  ]);
+  runScript = "bash";
+}).env
